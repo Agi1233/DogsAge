7c0acf1c20126ec97cd4db5a50690b07f4b6bd8a
Agi1233 <agnieszka.kopec1@wp.pl>
2014-11-18 09:29:01 +0100

    first commit

--- DogsAge/ViewController.swift
+++ DogsAge/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+
+    @IBOutlet weak var ageOfDogLabel: UILabel!
+    
+    
+    @IBOutlet weak var humanYearsTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,51 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func ConvertToDogYersButtonPressed(sender: UIButton) {
+        
+        let numberFromHumanYearsTextField = humanYearsTextField.text.toInt()!
+        
+        let ageConstant = 7
+        
+        
+        humanYearsTextField.resignFirstResponder()
+        
+        ageOfDogLabel.hidden = false
+        
+        ageOfDogLabel.text = "\(numberFromHumanYearsTextField * ageConstant)" + " Dog Years"
+        
+        
+    }
+    
+    
+    @IBAction func convertToRealDogYears(sender: UIButton) {
+        
+        var stringFromTextFiel = humanYearsTextField.text
+        var intFromTextField = stringFromTextFiel.toInt()!
+        var doubleStringFromTextField = Double((stringFromTextFiel as NSString).doubleValue)
+        
+        humanYearsTextField.resignFirstResponder()
+        ageOfDogLabel.hidden = false
+        
+        if doubleStringFromTextField <= 2 {
+            ageOfDogLabel.text = "\(doubleStringFromTextField * 10.5)"
+            
+        }
+       
+        else if (intFromTextField > 2) {
+            
+        ageOfDogLabel.text = "\(((intFromTextField - 2) * 4 + 21))"
+        
+        }
 
+        
+    }
+        
+    
+        
+        
 }
+
+
 
